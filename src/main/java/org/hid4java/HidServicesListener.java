450569a00d37f5f088c463bd302ff50270910b6b
Trent Summerlin <whirvis@ardenus.org>
2025-06-20 21:32:31 -0500

    Tidy up HidServicesListener.java

--- src/main/java/org/hid4java/HidServicesListener.java
+++ src/main/java/org/hid4java/HidServicesListener.java
@@ -1,7 +1,7 @@
 /*
- * The MIT License (MIT)
+ * the MIT License (MIT)
  *
- * Copyright (c) 2014-2015 Gary Rowe
+ * Copyright (c) 2014-2025 Gary Rowe, "Whirvis" Trent Summerlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,7 +10,7 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
+ * the above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -20,64 +20,82 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
-
 package org.hid4java;
 
 import org.hid4java.event.HidServicesEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.EventListener;
 
 /**
- * Interface to provide the following to API consumers:
- * <ul>
- * <li>Notification of a HID event</li>
- * </ul>
+ * Interface to listen for HID events.
  *
  * @since 0.0.1
  */
 public interface HidServicesListener extends EventListener {
-  /**
-   * A HID was attached
-   *
-   * @param event The event
-   */
-  void hidDeviceAttached(HidServicesEvent event);
 
-  /**
-   * A HID was detached
-   *
-   * @param event The event
-   */
-  void hidDeviceDetached(HidServicesEvent event);
+    /**
+     * Called when an HID device is attached.
+     * <p>
+     * <b>Default behavior:</b> No-op.
+     *
+     * @param event The event.
+     */
+    default void hidDeviceAttached(@NotNull HidServicesEvent event) {
+        /* no-op by default */
+    }
 
-  /**
-   * A HID failure occurred (enumeration, data transfer etc)
-   *
-   * @param event The event
-   */
-  void hidFailure(HidServicesEvent event);
+    /**
+     * Called when an HID device is detached.
+     * <p>
+     * <b>Default behavior:</b> No-op.
+     *
+     * @param event The event.
+     */
+    default void hidDeviceDetached(@NotNull HidServicesEvent event) {
+        /* no-op by default */
+    }
 
-  /**
-   * A HID input data buffer was populated
-   *
-   * @param event The event
-   */
-  void hidDataReceived(HidServicesEvent event);
+    /**
+     * Called when an HID failure occurs (enumeration, data transfer, etc.)
+     * <p>
+     * <b>Default behavior:</b> No-op.
+     *
+     * @param event The event.
+     */
+    default void hidFailure(@NotNull HidServicesEvent event) {
+        /* no-op by default */
+    }
 
-  /**
-   * An uncaught exception was thrown
-   * <p>
-   * <b>Note:</b> Any exceptions that occur in this callback will
-   * be silently ignored.
-   *
-   * @param event The event
-   * @param cause The uncaught exception
-   */
-  @SuppressWarnings({"unused", "CallToPrintStackTrace"})
-  default void hidListenerException(HidServicesEvent event, Throwable cause) {
-      /* TODO: more robust logging */
-      cause.printStackTrace();
-  }
+    /**
+     * Called when an HID input data buffer is populated.
+     * <p>
+     * <b>Default behavior:</b> No-op.
+     *
+     * @param event The event.
+     */
+    default void hidDataReceived(@NotNull HidServicesEvent event) {
+        /* no-op by default */
+    }
+
+    /**
+     * Called when an uncaught exception is thrown in a callback.
+     * To prevent a stack overflow, any exceptions that occur in this
+     * callback will only have their stack trace printed.
+     * <p>
+     * <b>Default behavior:</b> Prints the stack trace.
+     *
+     * @param event The event.
+     * @param cause The uncaught exception.
+     */
+    @SuppressWarnings({"unused", "CallToPrintStackTrace"})
+    default void hidListenerException(
+            @NotNull HidServicesEvent event,
+            @NotNull Throwable cause
+    ) {
+        /* TODO: more robust logging */
+        cause.printStackTrace();
+    }
+
 }
