0e78780866e33881f92dc8bdd284150a064777fe
Trent Summerlin <whirvis@ardenus.org>
2025-06-21 19:16:29 -0500

    Tidy up DarwinHidApiLibrary.java

--- src/main/java/org/hid4java/jna/DarwinHidApiLibrary.java
+++ src/main/java/org/hid4java/jna/DarwinHidApiLibrary.java
@@ -1,24 +1,55 @@
+/*
+ * the MIT License (MIT)
+ *
+ * Copyright (c) 2014-2025 Gary Rowe, "Whirvis" Trent Summerlin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * the above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.hid4java.jna;
 
 import com.sun.jna.Native;
 import com.sun.jna.WString;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * JNA proxy to the Darwin HID API library.
+ */
 public interface DarwinHidApiLibrary extends HidrawHidApiLibrary {
 
-  DarwinHidApiLibrary INSTANCE = Native.load("hidapi", DarwinHidApiLibrary.class);
+    @NotNull DarwinHidApiLibrary INSTANCE = Native.load(
+            "hidapi", DarwinHidApiLibrary.class);
 
-  /**
-   * Changes the behavior of all further calls to {@link #hid_open(short, short, WString)} or {@link #hid_open_path(String)}.
-   * <br>
-   * All devices opened by HIDAPI with {@link #hid_open(short, short, WString)} or {@link #hid_open_path(String)}
-   * are opened in exclusive mode per default.
-   * <br>
-   * Calling this function before {@link #hid_init()} or after {@link #hid_exit()} has no effect.
-   *
-   * @since hidapi 0.12.0
-   * @param openExclusive When set to 0 - all further devices will be opened in non-exclusive mode.
-   *                      Otherwise - all further devices will be opened in exclusive mode.
-   */
-  void hid_darwin_set_open_exclusive(int openExclusive);
+    /**
+     * Changes the behavior of opening HID devices.
+     * <p>
+     * By default, all HID devices are opened in exclusive mode.
+     * <p>
+     * <b>Note:</b> Calling this function before {@link #hid_init()}
+     * or after {@link #hid_exit()} has no effect.
+     *
+     * @param openExclusive Zero to have all further devices opened in
+     *                      non-exclusive mode. Any other value will have
+     *                      them opened in exclusive mode.
+     * @see #hid_open(short, short, WString)
+     * @see #hid_open_path(String)
+     * @since hidapi 0.12.0
+     */
+    void hid_darwin_set_open_exclusive(int openExclusive);
 
 }
