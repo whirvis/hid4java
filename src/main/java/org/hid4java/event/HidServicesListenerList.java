b7b1a49e74e3351acc1209e99e7c6e28e61ccf0d
Trent Summerlin <whirvis@ardenus.org>
2025-06-21 21:26:06 -0500

    Tidy up HidServicesListenerList.java

--- src/main/java/org/hid4java/event/HidServicesListenerList.java
+++ src/main/java/org/hid4java/event/HidServicesListenerList.java
@@ -1,7 +1,7 @@
 /*
- * The MIT License (MIT)
+ * the MIT License (MIT)
  *
- * Copyright (c) 2014-2015 Gary Rowe
+ * Copyright (c) 2014-2025 Gary Rowe, "Whirvis" Trent Summerlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,7 +10,7 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
+ * the above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -20,196 +20,211 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
-
 package org.hid4java.event;
 
 import org.hid4java.HidDevice;
 import org.hid4java.HidServicesListener;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.BiConsumer;
 
 /**
- * HID services listener list
+ * Container for {@link HidServicesListener} that can be used to
+ * fire events.
  *
  * @since 0.0.1
  */
 public class HidServicesListenerList {
 
-  /**
-   * The list with registered listeners
-   */
-  private final List<HidServicesListener> listeners = Collections.synchronizedList(new ArrayList<HidServicesListener>());
+    private final List<HidServicesListener> listeners;
+    private final ReadWriteLock listenersLock;
+    private final ExecutorService executorService;
 
-  private final ExecutorService executorService = Executors.newFixedThreadPool(
-    3, new ThreadFactory() {
-      @Override
-      public Thread newThread(Runnable runnable) {
-        Thread thread = Executors.defaultThreadFactory().newThread(runnable);
-        thread.setName("hid4java event worker");
-        thread.setDaemon(true);
-        return thread;
-      }
-    });
-
-  /**
-   * @param listener The listener to add
-   */
-  public final void add(final HidServicesListener listener) {
-    if (this.listeners.contains(listener)) {
-      return;
+    /**
+     * Constructs a new {@code HidServicesListenerList}.
+     */
+    public HidServicesListenerList() {
+        this.listeners = new ArrayList<>();
+        this.listenersLock = new ReentrantReadWriteLock();
+        this.executorService = createEventThreadPool(3);
     }
-    this.listeners.add(listener);
-  }
 
-  /**
-   * @param listener The listener to remove
-   */
-  public final void remove(final HidServicesListener listener) {
-    this.listeners.remove(listener);
-  }
+    /**
+     * Returns an unmodifiable view of the listeners.
+     *
+     * @return An unmodifiable view of the listeners.
+     */
+    public @NotNull List<HidServicesListener> getListeners() {
+        return Collections.unmodifiableList(listeners);
+    }
 
-  /**
-   * Removes all listeners
-   */
-  public final void clear() {
-    this.listeners.clear();
-  }
-
-  /**
-   * @return The listeners list
-   */
-  protected final List<HidServicesListener> getListeners() {
-    return this.listeners;
-  }
-
-  /**
-   * Returns an array with the currently registered listeners.
-   * The returned array is detached from the internal list of registered listeners.
-   *
-   * @return Array with registered listeners.
-   */
-  public HidServicesListener[] toArray() {
-    return getListeners().toArray(new HidServicesListener[0]);
-  }
-
-  /**
-   * Fire the HID device attached event
-   *
-   * @param hidDevice The device that was attached
-   */
-  public void fireHidDeviceAttached(final HidDevice hidDevice) {
-
-    // Broadcast on a different thread
-    executorService.submit(
-      new Runnable() {
-        @Override
-        public void run() {
-
-          HidServicesEvent event = new HidServicesEvent(hidDevice);
-
-          for (final HidServicesListener listener : toArray()) {
-            try {
-              listener.hidDeviceAttached(event);
-            } catch(Throwable cause) {
-              listener.hidListenerException(event, cause);
+    /**
+     * Adds a listener.
+     *
+     * @param listener The listener to add.
+     * @throws NullPointerException If {@code listener} is {@code null}.
+     */
+    public final void add(@NotNull HidServicesListener listener) {
+        Objects.requireNonNull(listener, "listener cannot be null");
+        listenersLock.writeLock().lock();
+        try {
+            if (!listeners.contains(listener)) {
+                listeners.add(listener);
             }
-          }
-
+        } finally {
+            listenersLock.writeLock().unlock();
         }
-      });
-  }
+    }
 
-  /**
-   * Fire the HID device detached event
-   *
-   * @param hidDevice The device that was detached
-   */
-  public void fireHidDeviceDetached(final HidDevice hidDevice) {
+    /**
+     * Removes a listener.
+     *
+     * @param listener The listener to remove.
+     */
+    public final void remove(@Nullable HidServicesListener listener) {
+        if (listener == null) {
+            return; /* don't bother with obtaining a lock */
+        }
+        listenersLock.writeLock().lock();
+        try {
+            listeners.remove(listener);
+        } finally {
+            listenersLock.writeLock().unlock();
+        }
+    }
 
-    // Broadcast on a different thread
-    executorService.submit(
-      new Runnable() {
-        @Override
-        public void run() {
+    /**
+     * Removes all listeners.
+     */
+    public final void clear() {
+        listenersLock.writeLock().lock();
+        try {
+            listeners.clear();
+        } finally {
+            listenersLock.writeLock().unlock();
+        }
+    }
 
-          HidServicesEvent event = new HidServicesEvent(hidDevice);
+    /*
+     * We use a copy to make sure no concurrent modifications occur while
+     * processing an event. Even if we obtain a lock to process the events,
+     * a listener removing itself (or adding another listener) could result
+     * in a deadlock.
+     */
+    private List<HidServicesListener> copyListeners() {
+        listenersLock.readLock().lock();
+        try {
+            List<HidServicesListener> copy = new ArrayList<>();
+            Collections.copy(listeners, copy);
+            return copy;
+        } finally {
+            listenersLock.readLock().unlock();
+        }
+    }
 
-          for (final HidServicesListener listener : toArray()) {
+    private void fireHidEvent(
+            HidServicesEvent event,
+            BiConsumer<HidServicesListener, HidServicesEvent> callback
+    ) {
+        List<HidServicesListener> listeners = this.copyListeners();
+        for (HidServicesListener listener : listeners) {
             try {
-              listener.hidDeviceDetached(event);
-            } catch(Throwable cause) {
-              listener.hidListenerException(event, cause);
+                callback.accept(listener, event);
+            } catch (Throwable cause) {
+                listener.hidListenerException(event, cause);
             }
-          }
-
         }
-      });
+    }
 
-  }
+    /**
+     * Fires the HID device attached event.
+     *
+     * @param hidDevice The device that was attached.
+     * @throws NullPointerException If {@code hidDevice} is {@code null}.
+     * @see HidServicesListener#hidDeviceAttached(HidServicesEvent)
+     */
+    public void fireHidDeviceAttached(@NotNull HidDevice hidDevice) {
+        Objects.requireNonNull(hidDevice, "hidDevice cannot be null");
+        executorService.submit(() -> {
+            HidServicesEvent event = new HidServicesEvent(hidDevice);
+            this.fireHidEvent(event, HidServicesListener::hidDeviceAttached);
+        });
+    }
 
-  /**
-   * Fire the HID failure event
-   *
-   * @param hidDevice The device that caused the error if known
-   */
-  public void fireHidFailure(final HidDevice hidDevice) {
+    /**
+     * Fires the HID device detached event.
+     *
+     * @param hidDevice The device that was detached.
+     * @throws NullPointerException If {@code hidDevice} is {@code null}.
+     * @see HidServicesListener#hidDeviceDetached(HidServicesEvent)
+     */
+    public void fireHidDeviceDetached(@NotNull HidDevice hidDevice) {
+        Objects.requireNonNull(hidDevice, "hidDevice cannot be null");
+        executorService.submit(() -> {
+            HidServicesEvent event = new HidServicesEvent(hidDevice);
+            this.fireHidEvent(event, HidServicesListener::hidDeviceDetached);
+        });
+    }
 
-    // Broadcast on a different thread
-    executorService.submit(
-      new Runnable() {
-        @Override
-        public void run() {
+    /**
+     * Fires the HID failure event.
+     *
+     * @param hidDevice The device that caused the error, {@code null}
+     *                  if the device is unknown.
+     * @see HidServicesListener#hidFailure(HidServicesEvent)
+     */
+    public void fireHidFailure(@Nullable HidDevice hidDevice) {
+        executorService.submit(() -> {
+            HidServicesEvent event = new HidServicesEvent(hidDevice);
+            this.fireHidEvent(event, HidServicesListener::hidFailure);
+        });
+    }
 
-          HidServicesEvent event = new HidServicesEvent(hidDevice);
+    /**
+     * Fires the HID data received event.
+     *
+     * @param hidDevice The device that sent data input.
+     * @param data      The buffer containing the received data.
+     * @throws NullPointerException If {@code hidDevice} or {@code data}
+     *                              are {@code null}.
+     * @see HidServicesListener#hidDataReceived(HidServicesEvent)
+     */
+    public void fireHidDataReceived(@NotNull HidDevice hidDevice, byte @NotNull [] data) {
+        Objects.requireNonNull(hidDevice, "hidDevice cannot be null");
+        Objects.requireNonNull(data, "data cannot be null");
+        executorService.submit(() -> {
+            HidServicesEvent event = new HidServicesEvent(hidDevice, data);
+            this.fireHidEvent(event, HidServicesListener::hidDataReceived);
+        });
+    }
 
-          for (final HidServicesListener listener : toArray()) {
-            try {
-              listener.hidFailure(event);
-            } catch(Throwable cause) {
-              listener.hidListenerException(event, cause);
-            }
-          }
+    private static Thread createEventThread(Runnable runnable) {
+        Thread eventThread = Executors
+                .defaultThreadFactory()
+                .newThread(runnable);
 
-        }
-      });
+        eventThread.setName("hid4java Event Worker");
+        eventThread.setDaemon(true);
 
-  }
+        return eventThread;
+    }
 
-  /**
-   * Fire the HID data received event
-   *
-   * @param hidDevice The device that triggered the data input
-   * @param dataReceived The buffer with the data received
-   */
-  public void fireHidDataReceived(final HidDevice hidDevice, final byte[] dataReceived) {
-
-    // Broadcast on a different thread
-    executorService.submit(
-      new Runnable() {
-        @Override
-        public void run() {
-
-          HidServicesEvent event = new HidServicesEvent(hidDevice, dataReceived);
-
-          for (final HidServicesListener listener : toArray()) {
-            try {
-              listener.hidDataReceived(event);
-            } catch(Throwable cause) {
-              listener.hidListenerException(event, cause);
-            }
-          }
-
-        }
-      });
-
-  }
+    @SuppressWarnings("SameParameterValue")
+    private static ExecutorService createEventThreadPool(int threadCount) {
+        return Executors.newFixedThreadPool(
+                threadCount,
+                HidServicesListenerList::createEventThread
+        );
+    }
 
 }
-
